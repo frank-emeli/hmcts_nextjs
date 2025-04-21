029c74efaf6c6e63a37b8d6601362bd96a20b453
Frank Emeli <frank.c.emeli@gmail.com>
2025-04-21 21:50:03 +0100

    Add header and footer

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center text-sm px-4 py-8 mt-10">
+            &copy; 2025 HMCTS. All rights reserved.
+        </footer>
+    );
+}
---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="bg-black text-white p-4">
+            <h1 className="text-l font-bold">
+                <span className="text-3xl mr-2">HMCTS</span>
+                Tasks Manager
+            </h1>
+        </header>
+    );
+}
